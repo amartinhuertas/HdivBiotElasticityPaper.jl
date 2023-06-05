35815f56b4511ceaf586d05dc0a5f96985f94e25
Documenter.jl <documenter@juliadocs.github.io>
2023-06-05 09:45:49 +0000

    build based on 21be87b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HdivBiotElasticityPaper","category":"page"},{"location":"#HdivBiotElasticityPaper","page":"Home","title":"HdivBiotElasticityPaper","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HdivBiotElasticityPaper.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HdivBiotElasticityPaper]","category":"page"}]
+}
